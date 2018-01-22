6867a5443668e4da31e792971f49d24c5629d58e
Justin Dachille <justindachille@gmail.com>
2018-01-21 23:17:21 -0800

    Started editing form - changed user doc to start out as user class

--- user.js
+++ user.js
@@ -1,13 +1,49 @@
 class User {
     constructor (firstName, lastName, id) {
-        this.firstName = firstName;
-        this.lastName = lastName;
+        this._firstName = firstName;
+        this._lastName = lastName;
         this._id = id;
     }
     
     get fullName() {
-        return this.generateFullName();
+        return this.firstName + ' ' + this.lastName;
     }
+	
+    set fullName (name) {
+        var words = name.toString().split(' ');
+        this.firstName = words[0] || '';
+        this.lastName = words[1] || '';
+    }
+	
+		get id () {
+			return this._id;
+		}
+	
+		set id (newId) {
+				if (newId) {
+					this._id = newId;
+				}
+		}
+	
+	get firstName () {
+		return this._firstName;
+	}
+	
+	set firstName (newFirstName) {
+		if (newFirstName) {
+			this.firstName = newFirstName;
+		}
+	}
+	
+	get lastName () {
+		return this._lastName;
+	}
+	
+	set lastName (newLastName) {
+		if (newLastName) {
+			this.lastName = newLastName;
+		}
+	}
     
     generateFullName () {
         if (firstName && lastName) {
